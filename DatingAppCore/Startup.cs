9cd6d76f09e51b1fef93d55e54f71321968b561a
Terin Sebastin <terin01@gmail.com>
2021-06-06 10:27:35 +0530

    InitialCommit

--- DatingAppCore/Startup.cs
+++ DatingAppCore/Startup.cs
@@ -20,8 +20,7 @@ namespace DatingAppCore
         public Startup(IConfiguration config)
         {
             _config = config;
-        }
-       
+        }       
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
