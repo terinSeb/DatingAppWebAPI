95cfe1b10932249ef255118ba8b54381c81f7b1e
Terin Sebastin <terin01@gmail.com>
2022-03-29 06:57:05 +0530

    Adding class for pagination

---
+++ DatingAppCore/Helpers/PagedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppCore.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            CurrentPage = pageNumber;
+            TotalPage = (int)Math.Ceiling(count / (double)pageSize);
+            PageSize = pageSize;
+            TotalCount = count;
+            AddRange(items);
+        }
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        //public static async 
+    }
+}
