d815ebbdedc8a6f483e73fa8336e74cddaf125eb
Terin Sebastin <terin01@gmail.com>
2022-03-30 06:52:18 +0530

    Adding Pagination Class

--- DatingAppCore/Helpers/PagedList.cs
+++ DatingAppCore/Helpers/PagedList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,6 +21,11 @@ namespace DatingAppCore.Helpers
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
 
-        //public static async 
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * 10).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
     }
 }
